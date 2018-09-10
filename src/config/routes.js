57a33cb4a19a1f2a6df465f75c065c8b371d0c99
David Bastos <davtechh@gmail.com>
2018-09-10 09:40:23 -0300

    Ajuste rota

--- src/config/routes.js
+++ src/config/routes.js
@@ -7,5 +7,5 @@ module.exports = function(server) {
     
     // TODO Routes
     const todoService = require('../api/todo/todoService')
-    todoService.register(router, '/todo')
+    todoService.register(router, '/todos')
 }
